93ba5cf8cb530a43c048f542d1d2eba546a2255a
MNS81 <0x4c0656dc@gmail.com>
2025-08-18 21:08:23 +0300

    Добавлено приложение CarriageHelper

---
+++ Components/Services/Carriage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppExample.Components.Services
+{
+    internal class Carriage
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Team { get; set; }
+        public string Number { get; set; }
+        public string Place { get; set; }
+        public string Weight { get; set; }
+        public string Cargo { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Components/Services/DataBase.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiAppExample.Components.Services
+{
+    internal class DataBase
+    {
+        public string conString = "server=triniti.ru-hoster.com;port=3306;username=mnszbaVi;password=8LcL8rs9i0;database=mnszbaVi;charset=utf8";
+    }
+}
